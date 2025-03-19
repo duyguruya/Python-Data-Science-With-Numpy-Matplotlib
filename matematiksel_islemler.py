04ca4aa59b66544e3b8848941f19ef2e413ba6d8
duyguruya <duyguruyacig1403@gmail.com>
2025-03-19 16:01:56 +0300

    Add files via upload

---
+++ matematiksel_islemler.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+dizi = np.array([10,20,30,40,50])
+
+print("Toplam: ",np.sum(dizi))
+print("Ortalama: ",np.mean(dizi))
+print("En Büyük: ",np.max(dizi))
+print("En Küçük: ",np.min(dizi))
+print("Standart Sapma: ",np.std(dizi))
+print("Varyans: ",np.var(dizi))
